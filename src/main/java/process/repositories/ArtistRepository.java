c747725f59950144b86d258f243388cb61e149c7
Matthew Julian <2072001@maranatha.ac.id>
2023-08-14 07:44:34 +0700

    update repo data

--- src/main/java/process/repositories/ArtistRepository.java
+++ src/main/java/process/repositories/ArtistRepository.java
@@ -1,4 +1,28 @@
 package process.repositories;
 
+import model.Artist;
+
 public class ArtistRepository {
+    private final Artist[] artists = {
+            new Artist("Afgan"),
+            new Artist("Coldplay"),
+            new Artist("Yura Yunita"),
+            new Artist("Tulus"),
+            new Artist("Tulus"),
+            new Artist("Juicy Luicy"),
+            new Artist("Hivi!"),
+            new Artist("Kodaline"),
+            new Artist("NIKI"),
+            new Artist("JVKE"),
+            new Artist("Coldplay"),
+            new Artist("Raim Laode"),
+            new Artist("Yura Yunita"),
+            new Artist("Soegi Bornean"),
+            new Artist("Sal Priadi")
+    };
+
+    // Getter method for the artists array
+    public Artist[] getArtists() {
+        return artists;
+    }
 }
--- src/main/java/process/usecases/artist/ArtistGet.java
+++ src/main/java/process/usecases/artist/ArtistGet.java
@@ -1,28 +1,13 @@
 package process.usecases.artist;
 
+import model.Album;
 import model.Artist;
+import process.repositories.AlbumRepository;
+import process.repositories.ArtistRepository;
 
 public class ArtistGet {
-    private final Artist[] artists = {
-            new Artist("Afgan"),
-            new Artist("Coldplay"),
-            new Artist("Yura Yunita"),
-            new Artist("Tulus"),
-            new Artist("Tulus"),
-            new Artist("Juicy Luicy"),
-            new Artist("Hivi!"),
-            new Artist("Kodaline"),
-            new Artist("NIKI"),
-            new Artist("JVKE"),
-            new Artist("Coldplay"),
-            new Artist("Raim Laode"),
-            new Artist("Yura Yunita"),
-            new Artist("Soegi Bornean"),
-            new Artist("Sal Priadi")
-    };
-
-    // Getter method for the artists array
-    public Artist[] getArtists() {
-        return artists;
+    public Artist[] exexuteArtistGet() {
+        ArtistRepository artistRepository = new ArtistRepository();
+        return artistRepository.getArtists();
     }
 }
