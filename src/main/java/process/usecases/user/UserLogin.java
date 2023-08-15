daa2974cbda46805cc231f2e78d1592ee4b8c1c0
Matthew Julian <2072001@maranatha.ac.id>
2023-08-15 11:29:36 +0700

    make connection

--- src/main/java/process/usecases/user/UserLogin.java
+++ src/main/java/process/usecases/user/UserLogin.java
@@ -5,7 +5,7 @@ import model.User;
 public class UserLogin {
     public User execute(String email, String password) {
         if (email.equals("2072001") && password.equals("2072001")) {
-            return new User("2072001", "2072001");
+            return new User("SV0001", "Matthew Julian","2072001@maranatha.ac.id","2072001");
         } else {
             return null;
         }
